92545927e0aee9ea0ffc66dd26617f9d1a570ef5
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-13 23:49:47 +0000

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,8 +9,8 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
+        self.__width = self.width = width
+        self.__height = self.height = height
         self.__x = x
         self.__y = y
         
