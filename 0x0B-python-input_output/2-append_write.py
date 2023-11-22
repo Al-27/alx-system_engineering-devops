c77e42f60ecaaa6304656ddcaa292349d2340a7f
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-22 23:39:27 +0000

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,8 +7,7 @@ def append_write(filename="", text=""):
     """
     ...
     """
-    f = Path(filename)
-    f.touch(exist_ok=True) 
     if filename != "":
         with open(filename,"a+",encoding="utf-8") as f:
+            f.touch(exist_ok=True) 
             f.write(text)
