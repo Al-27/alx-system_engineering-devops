844031c5f87da4ce3e0f0ec975efebf9e0e7c335
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-24 22:20:35 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,29 +3,18 @@
     ...
     ...
 """
-from sys import argv
-"""
-    ...
-    ...
-"""
+from sys import argv 
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
-"""
-    ...
-    ...
-"""
-fname = "add_item.json"
-"""
-    ...
-    ...
-"""
-f = open(fname, "a+")
-f.close()
-"""
-    ...
-    ...
-"""
-obj = load(fname)
-print(obj)
-obj += argv[1:] 
-save(obj,fname)
+if __name__ == "__main__":
+    """
+        ...
+        ...
+    """
+    fname = "add_item.json" 
+    f = open(fname, "a+")
+    f.close() 
+    obj = load(fname)
+    print(obj)
+    obj += argv[1:] 
+    save(obj,fname)
