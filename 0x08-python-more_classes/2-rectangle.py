bb249f1405cb2eaf0dd10fc57717be0974a7952b
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-24 04:14:45 +0000

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,8 +3,8 @@
 class Rectangle:
     """Rectangle"""
     def __init__(self, w=0, h=0):
-        self.width = w
-        self.height = h
+        self.__width = self.width = w
+        self.__height = self.height = h
     
     def width(self, value=None):
         if value == None:
