053ccc1c656571c5e2520853e2147410b7eb058c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-28 00:27:27 +0000

    Create 102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+/**
+ * main - 
+ * Return: int
+*/
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - 
+ * Return: int
+*/
+int main(void)
+{
+    int parenID = (int)getpid(), i = 0;
+
+    for(;i < 5; i++)
+    {
+        if(parenID == getpid())
+        {
+            fork();
+        }
+    }
+
+    if(parenID != getpid() )
+    {
+        printf("Zombie process created, PID: %d", (int)getpid());
+        infinite_while();
+    }
+    
+}
+
+    
