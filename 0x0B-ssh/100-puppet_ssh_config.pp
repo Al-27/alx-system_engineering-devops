433a3e92a4a2900f19265a0d30c650c77d502fc1
Al_27 <ahmedlaghfour@gmail.com>
2024-01-28 08:16:25 +0000

    ensure

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,7 @@
 #this is but a comment
+file { '/etc/ssh':
+  ensure => 'directory',
+}
 file{ "/etc/ssh/ssh_config":
   ensure  => 'present',
   content => 'HostName *\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
