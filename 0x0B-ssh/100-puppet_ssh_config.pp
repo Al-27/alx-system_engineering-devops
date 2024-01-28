57244d41a9913367f636305e5cba596f558d067b
Al_27 <ahmedlaghfour@gmail.com>
2024-01-28 08:04:40 +0000

    try

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,8 @@
 #this is but a comment
 file{ "${facts['home']}/.ssh/ssh_config":
   ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => 'HostName *\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
 }
