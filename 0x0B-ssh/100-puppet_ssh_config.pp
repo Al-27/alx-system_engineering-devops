f2499e4dab2ed97e6acccc5e97a8e663b3e219ec
Al_27 <ahmedlaghfour@gmail.com>
2024-01-28 08:06:54 +0000

    on

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,5 @@
 #this is but a comment
 file{ "/etc/ssh/ssh_config":
   ensure  => 'present',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   content => 'HostName *\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
 }
