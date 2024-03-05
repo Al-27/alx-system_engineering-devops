214435aa841f6557a851262ecd018bcd1862a2cf
Al_27 <ahmedlaghfour@gmail.com>
2024-03-05 20:24:48 +0000

    00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 from sys import argv
 
 def number_of_subscribers(subreddit):
-    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/about")
+    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/about",allow_redirects=False)
     try:
         subs = resp.json().get('data').get('subscribers')
         return subs if subs is not None else 0
