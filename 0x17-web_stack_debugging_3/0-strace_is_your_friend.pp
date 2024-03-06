f8f308e94c420f28323cb9b5f91362854c946a14
Al_27 <ahmedlaghfour@gmail.com>
2024-03-06 22:56:21 +0000

    locale

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes Apache 500 error
 
 exec { 'php Not parsed':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
   path    => '/usr/bin/:/usr/local/bin/:/bin/'
 }
