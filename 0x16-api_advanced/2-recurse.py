3dbb64ac1e12356f1faa847d59d684619c042c1a
Al-27 <ahmedlaghfour@gmail.com>
2024-03-07 12:30:41 -0800

    2Recurs

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,8 +6,9 @@ import requests
 
 
 def recurse(subreddit):
-    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/hot?limit=100",
-            headers={"HTTP_USER_AGENT": "checker:10.0.alx"}, allow_redirects=False)
+    url = f"https://api.reddit.com/r/{subreddit}/hot?limit=100"
+    hdrs = {"HTTP_USER_AGENT": "checker:10.0.alx"}
+    resp = requests.get(url ,headers=hdrs, allow_redirects=False)
     if resp.status_code != 200:
         return None
     try:
