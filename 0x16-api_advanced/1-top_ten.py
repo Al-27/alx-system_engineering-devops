03f11128f0776d19f3477366065f18831dc5c0f3
Al-27 <ahmedlaghfour@gmail.com>
2024-03-07 12:18:17 -0800

    2Recurs

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,13 +3,12 @@
 this is a doc
 """
 import requests
-from sys import argv
 
 def top_ten(subreddit):
-    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/hot", headers = {"HTTP_USER_AGENT": "chrome/10.0.0.1"} ,allow_redirects=False)
+    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/hot", headers = {"HTTP_USER_AGENT": "checker:10.0.alx"} ,allow_redirects=False)
     if resp.status_code != 200:
         print("None")
-        return
+        return None
     
     try:
         posts = resp.json().get("data").get("children")
@@ -18,3 +17,4 @@ def top_ten(subreddit):
             print(data.get('title'))
     except Exception as e: 
         print("None")
+
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this is a doc
+"""
+import requests
+
+def recurse(subreddit):
+    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/hot?limit=100", headers = {"HTTP_USER_AGENT": "checker:10.0.alx"} ,allow_redirects=False)
+    if resp.status_code != 200:
+        print("None")
+        return None
+    
+    try:
+        posts = resp.json().get("data").get("children")
+        list = [ p.get('data').get('title') for p in posts ]
+        return list
+    except Exception as e:
+        return None
+
