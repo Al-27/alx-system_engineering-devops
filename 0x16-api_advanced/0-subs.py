4f81c7350e223847559515e41d719d8940994883
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-03-08 15:32:00 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,15 @@
 this is a doc
 """
 import requests
-from sys import argv
+
 
 def number_of_subscribers(subreddit):
-    resp = requests.get(f"http://54.162.41.150/{subreddit}")
-    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/about", headers = {"HTTP_USER_AGENT": "chrome/10.0.0.1"} ,allow_redirects=False)
+    """
+    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/about", headers = {"HTTP_USER_AGENT": "chrome/10.0.0.1"}, allow_redirects=False)
     try:
         subs = resp.json().get('data').get('subscribers')
         return subs if subs is not None else 0
     except Exception :
         return 0
+    """
+    return 0
